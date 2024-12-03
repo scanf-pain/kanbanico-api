573851b78f9f5bce1bc42b9c7fc9db011fce17b9
scanf-pain <matus.cik@protonmail.com>
2024-12-03 12:47:24 +0100

    Init project

---
+++ src/main/java/com/kanbanico/api/KanbanicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kanbanico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanbanico/api/KanbanicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanbanico.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
